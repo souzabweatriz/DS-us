bbd54145a93345caa601a9ac11561d9bded560b0
Isabella Borin <isabellaborin.tds1.senai@gmail.com>
2024-11-21 16:58:20 -0300

    feat: adjust

--- Etapa-02/routes/chat.routes.js
+++ Etapa-02/routes/chat.routes.js
@@ -3,3 +3,9 @@ import ChatRepository from "../models/chat/ChatRepository.js";
 
 const chatRoutes = Router();
 const userList = new ChatRepository ();
+
+let chats = []
+
+chatRoutes.get
+
+export default chatRoutes;
